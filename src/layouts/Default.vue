02e011bef4695219bfa7cb9603cde747a34462e3
danielbonifacio <contato@danielbonifacio.com.br>
2019-09-03 17:42:02 -0300

    feat(default-layout): add header component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,13 +3,17 @@
     id="layout"
     class="default"
   >
+    <page-header />
     <slot />
   </div>
 </template>
 
 <script>
+import Header from 'Components/Header.vue'
 export default {
-
+  components: {
+    pageHeader: Header
+  }
 }
 </script>
 
