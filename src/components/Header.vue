f8e6a2e760166f9f009c3697aee8e6a733875ce4
danielbonifacio <contato@danielbonifacio.com.br>
2019-09-03 18:07:04 -0300

    feat(structure): add header block

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    todo: header
+    <!-- todo: header -->
   </header>
 </template>
 
@@ -9,3 +9,20 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~Styles/_vars.scss";
+@import "~Styles/_mixins.scss";
+
+$header_bg: $primary;
+$header_color: $foreground;
+$header_height: 70px;
+
+header {
+  background-color: $header_bg;
+  color: $header_color;
+  height: $header_height;
+  
+  @include align-center;
+}
+</style>
