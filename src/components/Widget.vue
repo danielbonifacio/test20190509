9644610c63ce433a8d50fe34b82bb8c442ec4a00
danielbonifacio <contato@danielbonifacio.com.br>
2019-09-03 19:45:57 -0300

    feat(company-page): add main widgets

---
+++ src/components/Widget.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="widget">
+    <div class="title">
+      {{ title }}
+    </div>
+    <div
+      class="body"
+      :class="{ 'no-padding': noPadding }"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    noPadding: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~Styles/_vars.scss";
+@import "~Styles/_mixins.scss";
+
+$border_color: lighten($primary, 50%);
+
+.widget {
+  background-color: $foreground;
+  border-radius: 5px;
+  border: 1px solid $border_color;
+
+  .title {
+    border-bottom: 1px solid $border_color;
+    padding: 1rem;
+
+    text-transform: uppercase;
+    font-size: .8rem;
+    font-weight: 600;
+    color: lighten($color, 30%);
+
+    // visual fix
+    letter-spacing: .02rem;
+  }
+
+  .body {
+    &:not(.no-padding) {
+      padding: 1rem;
+    }
+  }
+}
+</style>
--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -6,18 +6,45 @@
       :avatar="company.avatar"
       :description="company.description"
     />
-    todo: company page
+    <div id="content">
+      <widget
+        id="activities"
+        title="Activities"
+      >
+        slot
+      </widget>
+      <widget
+        id="similar"
+        title="Similar companies"
+      >
+        similar
+      </widget>
+      <widget
+        id="spent"
+        title="Company Spend History"
+      >
+        spent
+      </widget>
+      <widget
+        id="spent2"
+        title="Company Spend History"
+      >
+        spent2
+      </widget>
+    </div>
   </layout>
 </template>
 
 <script>
 import Default from 'Layouts/Default.vue'
 import CompanyBadge from 'Components/CompanyBadge.vue'
+import Widget from 'Components/Widget.vue'
 
 export default {
   components: {
     layout: Default,
-    CompanyBadge
+    CompanyBadge,
+    Widget
   },
   computed: {
     company() {
@@ -27,6 +54,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#content {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(auto-fill, 200px);
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
 
+  grid-template:
+    'activities similar'
+    'activities spent'
+    'activities spent2';
+
+  #activities { grid-area: activities }
+  #similar { grid-area: similar }
+  #spent { grid-area: spent }
+  #spent2 { grid-area: spent2 }
+}
 </style>
