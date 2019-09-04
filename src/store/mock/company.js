bde976a125b24e3edef509cffc3435ba02e64b85
danielbonifacio <contato@danielbonifacio.com.br>
2019-09-04 16:15:45 -0300

    feat(store): add activities to company mock

--- src/store/mock/company.js
+++ src/store/mock/company.js
@@ -2,5 +2,114 @@ export default {
   name: 'Microsoft',
   address: 'Redmond, WA, USA',
   avatar: 'https://www.freepnglogos.com/uploads/microsoft-window-logo-emblem-0.png',
-  description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid rerum velit ut similique saepe consequuntur possimus quas vel fugit optio?'
+  description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid rerum velit ut similique saepe consequuntur possimus quas vel fugit optio?',
+  activities: [
+    {
+      id: 1,
+      icon: 'video-platform',
+      date: '28/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Cloud Orchestration',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 2,
+      icon: 'content-delivery',
+      date: '26/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Express Scripts',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 3,
+      icon: 'waf-d-dos-security',
+      date: '24/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Kaiser Permanente',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 4,
+      icon: 'traffic-management',
+      date: '22/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Kaiser Permanente',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 5,
+      icon: 'dns',
+      date: '21/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Express Scripts',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 6,
+      icon: 'saas',
+      date: '20/08/2019',
+      operation: {
+        type: 'replace',
+        replace: {
+          title: 'Molina Healthcare',
+          url: '#'
+        },
+        property: {
+          amount: 1,
+          url: '#'
+        }
+      }
+    },
+    {
+      id: 7,
+      icon: 'content-delivery',
+      date: '20/08/2019',
+      operation: {
+        type: 'launch',
+        service: {
+          title: 'M-Cloud'
+        }
+      }
+    }
+  ]
 }
